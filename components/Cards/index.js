5ce545288a088df841823a20943003b31600935d
Cobrettie <cobrettiegarner3@gmail.com>
2020-02-21 20:05:19 -0500

    cards displaying correctly, axios request successful in ../Cards/index.js

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,13 +18,13 @@
 //
 // Create a card for each of the articles and add the card to the DOM.
 
-function cardCreator() {
+function cardCreator(headline, authorPhoto, authorName) {
   const mainCardDiv = document.createElement('div');
   mainCardDiv.classList.add('card');
 
   const headlineDiv = document.createElement('div');
   headlineDiv.classList.add('headline');
-  headlineDiv.textContent = 'headline of article';
+  headlineDiv.textContent = `${headline}`;
 
   const authorDiv = document.createElement('div');
   authorDiv.classList.add('author');
@@ -33,10 +33,10 @@ function cardCreator() {
   authorImgContainDiv.classList.add('img-container');
 
   const authorImg = document.createElement('img');
-  authorImg.alt = 'url of authors image'; // change to src, then fill in 'url of authors img'
+  authorImg.src = `${authorPhoto}`;
 
   const authorNameSpan = document.createElement('span');
-  authorNameSpan.textContent = 'authors name';
+  authorNameSpan.textContent = `${authorName}`;
 
   // append children :)
   mainCardDiv.appendChild(headlineDiv);
@@ -44,11 +44,46 @@ function cardCreator() {
   authorDiv.appendChild(authorImgContainDiv);
   authorImgContainDiv.appendChild(authorImg);
   authorDiv.appendChild(authorNameSpan);
+
+  return mainCardDiv;
 }
+
+const cardsContainer = document.querySelector('.cards-container');
 
 axios
   .get('https://lambda-times-backend.herokuapp.com/articles')
   .then(response => {
+    let jsArticles = response.data.articles.javascript;
+    jsArticles.forEach(item => {
+      let card = cardCreator(item.headline, item.authorPhoto, item.authorName);
+      cardsContainer.appendChild(card);
+    })
+
+    let bootstrapArticles = response.data.articles.bootstrap;
+    bootstrapArticles.forEach(item => {
+      let card = cardCreator(item.headline, item.authorPhoto, item.authorName);
+      cardsContainer.appendChild(card);
+    })
+
+    let technologyArticles = response.data.articles.technology;
+    technologyArticles.forEach(item => {
+      let card = cardCreator(item.headline, item.authorPhoto, item.authorName);
+      cardsContainer.appendChild(card);
+    })
+
+    let jQueryArticles = response.data.articles.jquery;
+    jQueryArticles.forEach(item => {
+      let card = cardCreator(item.headline, item.authorPhoto, item.authorName);
+      cardsContainer.appendChild(card);
+    })
+
+    let nodeArticles = response.data.articles.node;
+    nodeArticles.forEach(item => {
+      let card = cardCreator(item.headline, item.authorPhoto, item.authorName);
+      cardsContainer.appendChild(card);
+    })
+
+    console.log(jsArticles);
     console.log(response);
   })
   .catch(err => {
